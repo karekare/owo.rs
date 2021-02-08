4c8b8fb4bbb722a512d6c24ef801858a26e55817
utku <31706007+karekare@users.noreply.github.com>
2021-02-08 10:59:12 +0300

    Update active.js

--- active.js
+++ active.js
@@ -1,83 +1,14 @@
-/*
-* Notes:
-* 1) Please fill out all the information. If we need to contact you for legal purposes, we will do so through the email you provide.
-*    * If you are uncomfortable with sharing your email, you can share a link or reference to *two or more* social media accounts (e.g. twitter, discord, dev.to, etc.)
-*    * If your contact information is dead or unreachable, we may open your subdomain back up.
-* 2) Your domain has to match your project (with exceptions for special purposes)
-*    * For example, we would grant the username `eri` to a github account with the project name `eri2` or `eri.js` or `eri-rs` but not to the project name `maki`.
-* 3) There has to be a reasonable amount of content on your page. This is to make sure there is a purpose for it and to help stop potential abuse.
-*/
-
-let active = [{
-    "description": "The site for https://github.com/karekare/",
-    "domain": "melody.uwu.rs",
-    "contact": {
-        "username": "MolyMelody",
-        "email": "discord@basehorizon.tech"
+{
+    description: "My base in the internet."
+    domain: "melody.owo.rs", 
+    contact: { // you should have at least your github username and one other contact.
+        username: "karekare",
+        email: "utkusay95@gmail.com",
+        twitter: "@thequaken",
+        discord: "MoldyMelody#3969 (591567129594036225)"// tag
     },
-    "dns": {
-        "a": "redacted" // Redacted for privacy.
+    dns: { // you can have only *one* of these
+        cname: "basehorizon.tech",
+        a: "1.1.128.116" // your ipv4 address
     }
-}, {
-    "description": "My base in the internet.",
-    "domain": "melody.uwu.rs",
-    "contact": {
-        "gh_username": "karekare",
-        "email": "discord@basehorizon.tech"
-    },
-    "dns": {
-        "cname": "cname.vercel-dns.com"
-    }
-}, {
-    "description": "Redirection to uwu.rs.",
-    "domain": "owo.rs",
-    "contact": {
-        "username": "melody",
-        "email": "melody@uwu.rs"
-    },
-    "dns": {
-        "cname": "cname.vercel-dns.com"
-    }
-}, {
-    "description": "Private image subdomain.",
-    "domain": "i.owo.rs",
-    "contact": {
-        "username": "melody",
-        "email": "melody@uwu.rs"
-    },
-    "dns": {
-        "a": "redacted" // Redacted for privacy.
-        // If you want a redacted A record, mention that when you make your PR and we can help you.
-    }
-}, {
-    "description": "Private url shortener.",
-    "domain": "melody.owo.rs",
-    "contact": {
-        "username": "melody",
-        "email": "melody@uwu.rs"
-    },
-    "dns": {
-        "cname":"cname.vercel-dns.com
-    }
-},{
-    "description": "Personal site.",
-    "domain": "melody.owo.rs",
-    "contact": {
-        "username": "melody",
-        "twitter": "thequaken"
-    },
-    "dns": {
-        "cname": "melody.xyz"
-    }
-},{
-    "description": "My personel website in the internet.",
-    "domain": "melody.owo.rs",
-    "contact": {
-        "username": "melody",
-        "twitter": "@thequaken",
-        "discord": "MolyMelody#3969 (591567129594036225)"      
-    },
-    "dns": {
-        "cname":"melody.co.uk"
-    }
-},]
+},
